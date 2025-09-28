ba4c6c98a8cf0b82a85682fbaa07263b1c6bf668
CamBurnsSgm <cam@sweetgenmedia.com>
2025-09-28 15:27:02 +0100

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // The App Router is enabled by default in modern Next.js versions.
-  images: {
-    domains: []
-  }
-};
-
-module.exports = nextConfig;
