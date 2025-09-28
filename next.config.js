129ab2f042b78b9aea87797001209c4478625c0e
CamBurnsSgm <cam@sweetgenmedia.com>
2025-09-28 11:33:20 +0100

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // The App Router is enabled by default in modern Next.js versions.
+  images: {
+    domains: []
+  }
+};
+
+module.exports = nextConfig;
